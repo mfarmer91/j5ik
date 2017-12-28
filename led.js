2b85c85496e881b1eb63200f2f34293b55b3b374
Michael Farmer <mfarmer91@gmail.com>
2017-12-28 10:27:30 -0500

    blinking an LED

---
+++ led.js
@@ -0,0 +1,14 @@
+var Tessel = require("tessel-io");
+var five = require("johnny-five");
+//above are modules, which contains the code needed for tessel and johnny-five to run.
+
+var board = new five.Board({
+  io: new Tessel()
+})
+//creates a new instance object of a Johnny-Five board.
+
+board.on("ready", () => {
+  var led = new five.Led("a5");
+  led.blink(500);
+})
+//this executes when the board emits the "ready" event.  Gets executed "on" the board.
