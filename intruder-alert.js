59842962e72ac286b1cea4de4953c7c0378163be
Michael Farmer <mfarmer91@gmail.com>
2018-11-19 22:12:52 -0500

    updated intruder-alert.js with capability to use twilio to send an sms on switch open

--- intruder-alert.js
+++ intruder-alert.js
@@ -4,13 +4,31 @@ var board = new five.Board({
   io: new Tessel()
 });
 
+var accountSid = "";
+var authToken = "";
+
+var sender = "";
+var recipient = "";
+
+var client = new twilio.RestClient(accountSid, authToken);
+
 board.on("ready", () => {
-  var led = new five.Led("a5");
+  //var led = new five.Led("a5");
   var door = new five.Switch({
     pin: "a2",
     invert: true,
   });
 
-  door.on("open", () => led.on());
-  door.on("close", () => led.off());
+  door.on("open", () => {
+    body: `Security Breach at ${Date.now()}`
+    from: sender,
+    to: recipient,
+  });
+
+  client.messages.create(details, error => {
+    if (error) {
+      console.error(error.message);
+    }
+  });
+  //door.on("close", () => led.off());
 });
